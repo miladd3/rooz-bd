c382e5ff3e1a3e1a5fc443c032e8cc497b3d1137
miladd3 <milad.d3@gmail.com>
2022-07-23 12:23:32 +0200

    add confeti

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,89 @@
 <script setup>
+import JSConfetti from 'js-confetti';
 import Envelope from './components/Envelope.vue';
-import Image from "./components/Image.vue";
+import Image from './components/Image.vue';
+import Text from './components/Text.vue';
+import { ref } from 'vue';
+
+const jsConfetti = new JSConfetti();
+
+const addConfetti =  () => {
+  return new Promise((resolve) => {
+    jsConfetti.addConfetti().then(() => {
+      jsConfetti
+          .addConfetti({
+            emojis: ['🌈', '⚡️', '💥', '✨', '💫', '🌸'],
+          })
+          .then(() => {
+            jsConfetti.addConfetti({
+              confettiColors: [
+                '#ff0a54',
+                '#ff477e',
+                '#ff7096',
+                '#ff85a1',
+                '#fbb1bd',
+                '#f9bec7',
+              ],
+            })
+            resolve();
+          });
+    });
+  });
+};
+const isOpen = ref(false);
+const textIsOpen = ref(false);
+
+const onEnvelopeClick = () => {
+  isOpen.value = true;
+
+  addConfetti().then(() => {
+      textIsOpen.value = true;
+  });
+};
 </script>
 
 <template>
   <div class="app">
-    <!--  <Envelope />-->
-    <Image />
+    <Envelope @click="onEnvelopeClick" v-if="!isOpen" />
+    <template v-if="isOpen">
+      <Image />
+
+      <transition >
+        <div class="text-overlay" v-if="textIsOpen">
+          <Text />
+        </div>
+      </transition>
+    </template>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .app {
   width: 100%;
   height: 100vh;
+  position: relative;
+
+  .text-overlay {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: rgba(#000, 0.8);
+  }
+}
+
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
---
+++ src/components/Text.vue
@@ -0,0 +1,37 @@
+<script setup>
+</script>
+
+<template>
+  <div class="text">
+    <strong class="persian">تولدت مبارک رز</strong>
+
+    <p>
+      Happy birthday Rozemarijn, Thanks for being awesome and legendary and so much fun. I hope you have a great day. and great memories.
+    </p>
+
+    <p>
+      Wish you all the best.
+    </p>
+
+    <div class="milad">
+      Milad ❤️
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.text {
+  max-width: 80%;
+  text-align: center;
+
+  p {
+    display: block;
+    margin-bottom: 12px;
+    margin-top: 6px;
+  }
+
+  .persian {
+    font-size: 1.5rem;
+  }
+}
+</style>
