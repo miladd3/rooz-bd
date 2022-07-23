7ef4f3c224f442ab66f70005f947f53b91cdb671
miladd3 <milad.d3@gmail.com>
2022-07-23 12:05:25 +0200

    init

---
+++ src/components/Envelope.vue
@@ -0,0 +1,18 @@
+<script setup>
+import envelope from '../assets/env.png'
+</script>
+
+<template>
+  <div class="envelope">
+    <img :src="envelope" alt="envelope">
+  </div>
+</template>
+
+<style scoped lang="scss">
+.envelope {
+  img {
+    width: 100%;
+    box-sizing: border-box;
+  }
+}
+</style>
